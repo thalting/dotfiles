f85945db3d6ce12c66759fc599bc51d5f7fe4b8b
halting <halting@riseup.net>
2022-06-10 02:00:44 -0300

    update tym config

--- .config/tym/config.lua
+++ .config/tym/config.lua
@@ -2,18 +2,29 @@ local tym = require("tym")
 
 tym.set_config({
     font = "Cozette",
+    rewrap = true,
     isolated = true,
-    padding_horizontal = 20,
-    padding_vertical = 20,
+    padding_horizontal = 22,
+    padding_vertical = 22,
     color_window_background = "#0c0c0d",
     cursor_shape = "ibeam",
 })
 
 tym.set_keymaps({
-    ["<Shift>j"] = function()
+    ["<Control><Shift>j"] = function()
+        for _ = 1, 10 do
+            tym.send_key("<Control><Shift>Down")
+        end
+    end,
+    ["<Control><Shift>k"] = function()
+        for _ = 1, 10 do
+            tym.send_key("<Control><Shift>Up")
+        end
+    end,
+    ["<Control>j"] = function()
         tym.send_key("<Control><Shift>Down")
     end,
-    ["<Shift>k"] = function()
+    ["<Control>k"] = function()
         tym.send_key("<Control><Shift>Up")
     end,
     ["<Control>y"] = function()
