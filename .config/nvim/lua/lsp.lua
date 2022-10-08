c5f4a16d834c70b5a1c37898805b7ac7c76a26db
halting <halting@riseup.net>
2022-10-08 18:31:51 -0300

    luasnip: disable history

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -112,3 +112,17 @@ cmp.setup.cmdline('/', {
         { name = 'buffer' }
     }
 })
+
+function LeaveSnippet()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+        require('luasnip').unlink_current()
+    end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+   autocmd ModeChanged * lua LeaveSnippet()
+]])
