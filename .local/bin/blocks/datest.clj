1835748ce0fac9bb53e5d17ce3ca6a3740879d94
halting <halting@riseup.net>
2022-10-14 14:12:27 -0300

    update date block

--- .local/bin/blocks/datest.clj
+++ .local/bin/blocks/datest.clj
@@ -1,4 +1,4 @@
 #!/usr/bin/env bb
 
-(def date (.format (java.text.SimpleDateFormat. "E, d MMM y, h:m a") (new java.util.Date)))
-(println date)
+(def date (.format (java.text.SimpleDateFormat. "E, d MMM y, hh:mm a") (new java.util.Date)))
+(print date)
