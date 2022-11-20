c769fb669303771b84c827fb16d17019e78023f4
halting <halting@riseup.net>
2022-11-20 20:50:09 -0300

    volst wpctl

--- .local/bin/blocks/volst.clj
+++ .local/bin/blocks/volst.clj
@@ -1,12 +1,19 @@
 #!/usr/bin/env bb
 
 (require '[clojure.java.shell :refer [sh]])
+(require '[clojure.string])
 
-(let [vol (read-string (-> (sh "pamixer" "--get-volume")
-                           :out))
-      state (read-string (-> (sh "pamixer" "--get-mute")
-                             :out))]
-  (if (or (= state true) (= vol 0))
+(let [info (clojure.string/split
+            (-> (sh "wpctl" "get-volume" "@DEFAULT_AUDIO_SINK@")
+                :out)
+            #"\n")
+      vol
+      (read-string (get (clojure.string/split
+                         (-> (sh "wpctl" "get-volume" "@DEFAULT_AUDIO_SINK@")
+                             :out)
+                         #"\.")
+                        1))]
+  (if (or (.contains (str (get info 0) "\n") "MUTE") (= vol 0))
     (print "婢")
     (if (< vol 33)
       (printf "%s%%" vol)
