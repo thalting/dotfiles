f93220131dd43a5c8fe8f474ba8373bfec6effd6
halting <halting@riseup.net>
2023-05-16 10:43:09 -0300

    update

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,13 +1,12 @@
 {- ORMOLU_DISABLE -}
 import XMonad
-import XMonad.StackSet (focusWindow, greedyView, shift, swapMaster, shiftMaster)
+import XMonad.StackSet (focusWindow, greedyView, shift, swapMaster, shiftMaster, sink)
 
 -- Actions
 import qualified XMonad.Actions.FlexibleResize as Flex
 import XMonad.Actions.CycleWS (nextWS, prevWS, toggleWS)
 import XMonad.Actions.EasyMotion (EasyMotionConfig (..), fixedSize, selectWindow)
 import XMonad.Actions.Promote (promote)
-import XMonad.Actions.CopyWindow (copy, copiesPP, kill1)
 import XMonad.Actions.SinkAll
 import XMonad.Actions.TiledWindowDragging
 import XMonad.Layout.DraggingVisualizer
@@ -26,9 +25,10 @@ import XMonad.Hooks.UrgencyHook (NoUrgencyHook (NoUrgencyHook), withUrgencyHook)
 import XMonad.Layout.NoBorders (Ambiguity (OnlyScreenFloat), lessBorders)
 import XMonad.Layout.PositionStoreFloat (positionStoreFloat)
 import XMonad.Layout.Renamed (Rename (CutWordsLeft, Replace), renamed)
-import XMonad.Layout.Spacing (Border (Border), spacingRaw, incScreenSpacing, decScreenSpacing, incWindowSpacing, decWindowSpacing)
+import XMonad.Layout.Spacing (Border (Border), spacingRaw, incScreenSpacing, decScreenSpacing, incWindowSpacing, decWindowSpacing, setScreenWindowSpacing)
 
 -- Util
+import qualified XMonad.Util.Hacks as Hacks
 import XMonad.Util.ActionCycle (cycleAction)
 import XMonad.Util.Cursor (setDefaultCursor)
 import XMonad.Util.EZConfig (additionalKeysP)
@@ -54,6 +54,8 @@ myFocusedBorderColor = "#d8d8d8"
 myFont = "xft:Cozette"
 
 myFocusFollowsMouse = False
+
+myXmobarCMD = "cleanup() { trap : TERM; kill 0; }; trap cleanup EXIT; xmobar"
 
 myLayout = fullscreenNoBorders $ trimWordLeft $ gaps gapsWidth layouts
   where
@@ -91,7 +93,7 @@ myEMConf =
 myKeys =
   [ ("M-<Return>", spawn myTerminal),
     ("M-p", spawn "drun"),
-    ("M-q", kill1),
+    ("M-q", kill),
     ("M-S-r", spawn "xmonad --recompile && xmonad --restart"),
     ("M-S-<Return>", promote),
 
@@ -105,9 +107,9 @@ myKeys =
     ("M-S-0", windows $ shift $ last myWorkspaces),
 
     -- Layouts
-    ("M1-t", sendMessage $ JumpToLayout "Tiled"),
-    ("M1-f", sendMessage $ JumpToLayout "Floating"),
-    ("M1-m", sendMessage $ JumpToLayout "Monocle"),
+    ("M-t", sendMessage $ JumpToLayout "Tiled"),
+    ("M-f", sendMessage $ JumpToLayout "Floating"),
+    ("M-m", sendMessage $ JumpToLayout "Monocle"),
 
     -- Easy Motion
     ("M-\\", selectWindow myEMConf >>= (`whenJust` windows . focusWindow)),
@@ -116,36 +118,27 @@ myKeys =
     ("M-S-f", setMode floatModeLabel),
 
     -- Toggle Xmobar
-    ("M-S-b", cycleAction "toggleXmobar" [killStatusBar "xmobar", spawnStatusBar "xmobar"]),
+    ("M-S-b", cycleAction "toggleXmobar" [killStatusBar myXmobarCMD, spawnStatusBar myXmobarCMD]),
 
     -- Toggle Picom
-    ("M-S-p", cycleAction "togglePicom" [spawn "notify-send 'Picom: OFF' && pkill picom", spawn "notify-send 'Picom: ON' && picom"]),
+    ("M-S-C-p", cycleAction "togglePicom" [spawn "notify-send 'Picom: OFF' && pkill picom", spawn "notify-send 'Picom: ON' && picom"]),
 
     -- Push all window back into tiling
-    ("M-S-t", sinkAll),
-
-    -- Copy Window
-    ("M-C-1", windows $ copy $ head myWorkspaces),
-    ("M-C-2", windows $ copy $ myWorkspaces !! 1),
-    ("M-C-3", windows $ copy $ myWorkspaces !! 2),
-    ("M-C-4", windows $ copy $ myWorkspaces !! 3),
-    ("M-C-5", windows $ copy $ myWorkspaces !! 4),
-    ("M-C-6", windows $ copy $ myWorkspaces !! 5),
-    ("M-C-7", windows $ copy $ myWorkspaces !! 6),
-    ("M-C-8", windows $ copy $ myWorkspaces !! 7),
-    ("M-C-9", windows $ copy $ myWorkspaces !! 8),
-    ("M-C-0", windows $ copy $ last myWorkspaces),
+    ("M-S-C-<Space>", sinkAll),
+    -- Push window back into tiling
+    ("M-S-<Space>", withFocused $ windows . sink),
 
     -- Spacing
+    ("M-C-S-r", setScreenWindowSpacing 5),
     ("M-C-S-k", incScreenSpacing 5),
     ("M-C-S-j", decScreenSpacing 5),
     ("M-C-S-l", incWindowSpacing 5),
     ("M-C-S-h", decWindowSpacing 5),
 
     -- Media Keys
-    ("<XF86AudioRaiseVolume>", spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"),
-    ("<XF86AudioLowerVolume>", spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"),
-    ("<XF86AudioMute>", spawn "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"),
+    ("<XF86AudioRaiseVolume>", spawn "wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+ && pkill -RTMIN+10 blocks"),
+    ("<XF86AudioLowerVolume>", spawn "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && pkill -RTMIN+10 blocks"),
+    ("<XF86AudioMute>", spawn "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle && pkill -RTMIN+10 blocks"),
     ("<XF86AudioPrev>", spawn "playerctl previous"),
     ("<XF86AudioPlay>", spawn "playerctl play-pause"),
     ("<XF86AudioNext>", spawn "playerctl next"),
@@ -194,12 +187,18 @@ myManageHook =
       ]
     <> composeAll
       [ className =? "Xmessage" --> doCenterFloat,
+        title =? "Picture-in-Picture" --> doFloat,
         checkDock --> doLower
       ]
 
-myHandleEventHook = positionStoreEventHook
+myHandleEventHook =
+  handleEventHook def
+    <> positionStoreEventHook
+    <> Hacks.windowedFullscreenFixEventHook
 
-myStartupHook = setDefaultCursor xC_left_ptr
+myStartupHook = do
+  setDefaultCursor xC_left_ptr
+  spawn "setxkbmap -option compose:ralt"
 
 myConfig =
   def
@@ -224,5 +223,5 @@ main =
     . ewmh
     . withUrgencyHook NoUrgencyHook
     . modal [floatMode 10]
-    . withEasySB (statusBarProp "xmobar" (copiesPP (pad . xmobarColor "#f1fa8c" "") myXmobarPP)) defToggleStrutsKey
+    . withEasySB (statusBarProp myXmobarCMD (pure myXmobarPP)) defToggleStrutsKey
     $ myConfig
