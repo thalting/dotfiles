8740cc9116a06e34a044cefa0265ebd5b6bd165e
halting <halting@riseup.net>
2025-03-13 12:22:01 -0300

    redirect stderr to /dev/null in volst

--- .local/bin/blocks/volst.lua
+++ .local/bin/blocks/volst.lua
@@ -1,6 +1,6 @@
 #!/bin/luajit
 
-local handle = io.popen("wpctl get-volume @DEFAULT_AUDIO_SINK@")
+local handle = io.popen("wpctl get-volume @DEFAULT_AUDIO_SINK@ 2> /dev/null")
 
 local str
 if handle ~= nil then
